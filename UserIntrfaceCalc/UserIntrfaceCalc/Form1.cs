2649616b805c7ed7e48a930c651133e3eb4556a2
ahmedsafwat101 <ahmedsafwat172@gmail.com>
2020-11-05 12:56:24 +0200

    Enhance the logic

--- UserIntrfaceCalc/UserIntrfaceCalc/Form1.cs
+++ UserIntrfaceCalc/UserIntrfaceCalc/Form1.cs
@@ -38,17 +38,17 @@ namespace UserIntrfaceCalc
 
         private void button1_Click(object sender, EventArgs e)
         {
-            ParsingInput(textBox1.Text, textBox2.Text,'+');
+            ParsingInput(textBox1.Text, textBox2.Text, '+');
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            ParsingInput(textBox1.Text, textBox2.Text,'-');
+            ParsingInput(textBox1.Text, textBox2.Text, '-');
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            ParsingInput(textBox1.Text, textBox2.Text,'*');
+            ParsingInput(textBox1.Text, textBox2.Text, '*');
 
         }
         private void button4_Click(object sender, EventArgs e)
@@ -57,7 +57,15 @@ namespace UserIntrfaceCalc
 
         }
         /**Logic of Consuming Service **/
-        private void ParsingInput(String num1Asstring, String num2Asstring, char sign) {
+        private void ParsingInput(String num1Asstring, String num2Asstring, char sign)
+        {
+
+            if (notValidInput(num1Asstring,num2Asstring))
+            {
+                MessageBox.Show("Please, Enter valid input");
+                return;
+            }
+
             int num1 = int.Parse(num1Asstring);
             int num2 = int.Parse(num2Asstring);
 
@@ -67,12 +75,30 @@ namespace UserIntrfaceCalc
                 textBox3.Text = (Ref.Subtraction(num1, num2)).ToString();
             else if (sign == '*')
                 textBox3.Text = (Ref.Multiplication(num1, num2)).ToString();
-            else if (sign == '/' && num1 != 0)
+            else if (sign == '/' && num2 != 0)
                 textBox3.Text = (Ref.Subtraction(num1, num2)).ToString();
             else
                 MessageBox.Show("Cannot divide on Zero");
         }
+        /** Check Validation **/
+        private Boolean notValidInput(String num1Asstring, String num2Asstring)
+        {
+            if (String.IsNullOrWhiteSpace(num1Asstring) || String.IsNullOrEmpty(num2Asstring))
+            {
+                return true;
+            }
 
-       
+            for (int i = 0; i < num1Asstring.Length; i++)
+            {
+                if (Char.IsLetter(num1Asstring[i])) return true;
+            }
+
+            for (int i = 0; i < num2Asstring.Length; i++)
+            {
+                if (Char.IsLetter(num2Asstring[i])) return true;
+            }
+
+            return false;
+        }
     }
 }
